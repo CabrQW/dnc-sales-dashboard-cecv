24a53074e0bea872ea4c113900911e6b9d560507
CabrQW <carlosbom64@gmail.com>
2025-07-30 17:38:44 -0300

    Feat: Chart Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 //COMPONENTS
-import { AvatarsList, CustomTable, CardComponent, Header } from "@/components" 
+import { AvatarsList, CustomTable, CardComponent, CustomChart , Header } from "@/components" 
 
 //MUI
 import { Container } from "@mui/system"
@@ -44,6 +44,13 @@ function Home() {
         </CardComponent>
         <CardComponent>
           <CustomTable headers={mockTableData.headers} rows={mockTableData.rows}/>
+        </CardComponent>
+        <CardComponent>
+          <CustomChart 
+          labels={['Jan', 'Fev', 'Mar', 'Abr', 'Mai']} 
+          data={[1000.12, 5641.3, 4567.65, 5000.01, 5000.00, 5500.12]} 
+          type="bar"
+          />
         </CardComponent>
       </Container>
     </>
