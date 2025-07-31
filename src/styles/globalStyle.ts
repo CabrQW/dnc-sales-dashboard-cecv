4205f7726de994eb960edf5a54b850785ba42fbf
CabrQW <carlosbom64@gmail.com>
2025-07-31 19:24:48 -0300

    Feat: Logout Flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 import type { DefaultTheme } from 'styled-components'
 
+//UTILS
+import { pxToRem } from '@/utils'
+
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
         body, html { 
         background: ${(props) => props.theme.appBackground};
@@ -13,5 +16,12 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
         margin: 0; 
         padding: 0; 
         
+        }
+        .mb-1 {
+        margin-bottom: ${pxToRem(16)};
+        }
+
+        .mb-2 {
+        margin-bottom: ${pxToRem(32)};
         }
 `
