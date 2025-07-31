354bf04a953bc52dd2e5fec4b665e8ccc7fd0051
CabrQW <carlosbom64@gmail.com>
2025-07-31 18:51:46 -0300

    Feat:login page requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom'
 import { Home, Leads, Login,Profiles, Registration } from './pages'
+import Cookies from 'js-cookie'
 
 function App() {
+  const ProtectadRoute = () => {
+    const checkAuthCookie = Cookies.get('Authorization')
+    if (!checkAuthCookie) {
+      alert('Autenticação necessaria')
+      return <Navigate to="/" replace />
+    }
+
+    return <Outlet />
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Registration />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/leads" element={<Leads />} />
-        <Route path="/perfil" element={<Profiles />} />
+        <Route element={<ProtectadRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/leads" element={<Leads />} />
+          <Route path="/perfil" element={<Profiles />} />
+        </Route>
       </Routes>
     </Router>
   )
